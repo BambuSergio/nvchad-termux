a0e16a7b44179c107dae984c9de6ed3640ed4db5
BambuSergio <sergio.tm.madrid@gmail.com>
2025-10-29 02:52:17 +0100

    Atajo de teclado para aplicar negrita en Markdown a palabra bajo el cursor en modo normal o a texto seleccionado en modo visual

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,14 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- ATAJOS MARKDOWN
+
+-- Modo normal: aplica negrita a la palabra bajo el cursor
+map("n", "<leader>mb", [[yiwciw**<C-r>"**<Esc>]], { desc = "Negrita markdown a palabra" })
+
+map("n", "<leader>mb", [[yiwciw**<C-r>"**<Esc>]], { desc = "Negrita Markdown a palabra" })
+
+-- Modo visual: aplica negrita al texto seleccionado
+map("v", "<leader>mb", [[c**<C-r>"**<Esc>]], { desc = "Negrita Markdown a selección" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
